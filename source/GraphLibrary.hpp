3358ec102ae1ebd35fec13100c0faf2eabbd88b0
AmandaACLucio <amandalucio@poli.ufrj.br>
2021-05-30 19:41:09 -0300

    Resultados ignore

--- source/GraphLibrary.hpp
+++ source/GraphLibrary.hpp
@@ -30,18 +30,18 @@ class Grafo
     Grafo(string caminho, int estrutura, bool newPeso, bool newDirecionado);
     void Entrada(string fileEntrada);
 
-    void Saida(string fileSaida="graphSaida.txt");
+    void Saida(string fileSaida="./Resultados/graphSaida.txt");
     vector<vector<int>> BFS(int raiz);
     vector<vector<int>> DFS(int raiz);
     int Grau(int vertice);
     float Distancia(int nodeUm, int nodeDois); //alterado para aceitar peso
     void Diametro();
-    vector<vector<int>> ComponentesConexas(string filesaida="graphComponentesConexas.txt");
+    vector<vector<int>> ComponentesConexas(string filesaida="./Resultados/graphComponentesConexas.txt");
     bool MesmaComponente(int nodeUm, int nodeDois);
 
     //TP2
-    pair<float, vector<float>> Dijkstra(int nodeUm, int nodeDois, string filesaida="Dijkstra.txt"); //caminho mínimo
-    void MST(int inicio=1, string filesaida="MST.txt"); //escrita no formato de grafo em um arquivo de saída
+    pair<float, vector<float>> Dijkstra(int nodeUm, int nodeDois, string filesaida="./Resultados/Dijkstra.txt"); //caminho mínimo
+    void MST(int inicio=1, string filesaida="./Resultados/MST.txt"); //escrita no formato de grafo em um arquivo de saída
     float Excentricidade(int nodeUm);
     void EscreveNovoGrafo(vector<pair<int,float>> pai, vector<int> nivel, string filesaida, int custoTotal);
 
